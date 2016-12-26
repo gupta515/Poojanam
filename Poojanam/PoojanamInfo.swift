49e3ada6d671d8559748337ac2d888ec221bd586
Samba Siva Gupta <gupta.venkataraman@a-cti.com>
2016-12-27 00:57:18 +0530

    -Stotram List View Ui changes

--- Poojanam/PoojanamInfo.swift
+++ Poojanam/PoojanamInfo.swift
@@ -12,6 +12,8 @@ let languagesAvailable = ["Hindi","Marathi","Telugu","Tamil"]
 
 let satyanarayanapoojavidhi = "https://firebasestorage.googleapis.com/v0/b/poojanam-d5278.appspot.com/o/audio%2Fmarathi%2Fpoojavidhi%2Fsatyanarayanpooja%20.m4a?alt=media&token=48bfc778-afa6-48e7-9ebf-48defd4fa8d8"
 
+//Mark:- Pooja's INFO
+
 // Marathi Pooja's
 let marathi_pooja_satyanarayana : [String:String] = ["about":"marathi_satyanarayan_pooja_about", "audio":"marathi_satyanarayan_pooja", "benefits":"marathi_satyanarayan_pooja_benefits", "duration":"01hr 15mins", "name":"Satyanarayana Pooja",
     "image":"satyanarayana",
@@ -28,4 +30,40 @@ let marathi_poojas : [String:[String:String]] = ["Satyanarayana Pooja":marathi_p
 let poojaDataDicts : [String:[String:String]] = ["Satyanarayana Pooja":marathi_pooja_satyanarayana, "Mangalagauri Pooja":marathi_pooja_mangalagauri, "Parthiv Ganapathi": marathi_pooja_parthivGanapathi]
 
 let allPoojas : [String:[String:[String:String]]] = ["marathi":marathi_poojas]
+
+//MARK:- Stotram INFO
+
+//MARK:- Marathi Stotrams8
+let marathi_stotram_ganpati_atharvashisha : [String:String] = ["audio":"marathi_ganpati_athravshisha_stotram", "duration":"07mins"]
+
+let marathi_stotram_ganpati_sukta : [String:String] = ["audio":"marathi_ganpati_sukta_stotram", "duration":"03mins"]
+
+let marathi_stotram_mahalakshmi_ashtakam : [String:String] = ["audio":"marathi_mahalakshmi_ashtakam_stotram", "duration":"03mins"]
+
+let marathi_stotram_maruti : [String:String] = ["audio":"marathi_maruti_stotram", "duration":"03mins"]
+
+let marathi_stotram_navgraha : [String:String] = ["audio":"marathi_navgraha_stotram", "duration":"03mins"]
+
+let marathi_stotram_purusha_sukta : [String:String] = ["audio":"marathi_purusha_sukta_stotram", "duration":"07mins"]
+
+let marathi_stotram_ramaraksha : [String:String] = ["audio":"marathi_ramaraksha_stotram", "duration":"06mins"]
+
+let marathi_stotram_rudra_adhyay : [String:String] = ["audio":"marathi_rudra_adhyay_stotram", "duration":"23mins"]
+
+let marathi_stotram_sankatnashan_ganesh : [String:String] = ["audio":"marathi_sankatnashan_ganesh_stotram", "duration":"02mins"]
+
+let marathi_stotram_shani : [String:String] = ["audio":"marathi_shani_stotram", "duration":"04mins"]
+
+let marathi_stotram_shri_stotram : [String:String] = ["audio":"marathi_shri_stotram", "duration":"08mins"]
+
+let marathi_stotram_sour : [String:String] = ["audio":"marathi_sour_stotram", "duration":"15mins"]
+
+let marathi_stotram_dattatreya_kashta_nashak : [String:String] = ["audio":"marathi_dattatreya_kashta_nashak_stotram", "duration":"03mins"]
+
+let marathi_stotrams : [String] = ["Ganpati Atharvashisha", "Ganpati Sukta", "Mahalakshmi Ashtak", "Maruti Stotram", "Navgraha Stotram", "Purusha Sukta", "Ramraksha Stotra", "Rudra Adhyay", "Sankat Nashan GaneshStotra", "Shani Stotra", "Shri Sukta", "Sour Sukta", "Dattatreya Kashta Nashak"]
+
+let stotramDataDict : [String: [String:String]] = ["Ganpati Atharvashisha": marathi_stotram_ganpati_atharvashisha, "Ganpati Sukta": marathi_stotram_ganpati_sukta, "Mahalakshmi Ashtak": marathi_stotram_mahalakshmi_ashtakam, "Maruti Stotram":marathi_stotram_maruti, "Navgraha Stotram": marathi_stotram_navgraha, "Purusha Sukta": marathi_stotram_purusha_sukta, "Ramraksha Stotra": marathi_stotram_ramaraksha, "Rudra Adhyay": marathi_stotram_rudra_adhyay, "Sankat Nashan GaneshStotra": marathi_stotram_sankatnashan_ganesh, "Shani Stotra": marathi_stotram_shani, "Shri Sukta": marathi_stotram_shri_stotram, "Sour Sukta": marathi_stotram_sour, "Dattatreya Kashta Nashak": marathi_stotram_dattatreya_kashta_nashak]
+
+let allStotrams : [String: [String]] = ["marathi" :  marathi_stotrams]
+
 
--- Poojanam/StotramViewController.swift
+++ Poojanam/StotramViewController.swift
@@ -20,14 +20,21 @@ class StotramViewController: UIViewController, UITableViewDataSource, UITableVie
     
     var isStotramPlaying = false
     var selectedStotramIndex = 0
+    
+    var langStotrams : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.navigationItem.title = "Stotram"
+        
+        if let selectedLang = UserDefaults.standard.string(forKey: "selectedLanguage"), let reqStotramList = allStotrams[selectedLang.lowercased()]{
+            langStotrams = reqStotramList.sorted()
+            stotramTableView.reloadData()
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -40,14 +47,16 @@ class StotramViewController: UIViewController, UITableViewDataSource, UITableVie
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return langStotrams.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let stotramCell = tableView.dequeueReusableCell(withIdentifier: "StotramCellID") as!StotramTableViewCell
-        stotramCell.stotramNameLabel.text = ("Ayyappa Stotram \(indexPath.row + 1)")
-        stotramCell.stotramProgressLabel.text = "5 mins"
-        
+        if let stotramData = stotramDataDict[langStotrams[indexPath.row]] {
+            stotramCell.stotramNameLabel.text = langStotrams[indexPath.row]
+            stotramCell.stotramProgressLabel.text = stotramData["duration"]
+        }
+
         //Play Button Actions
         stotramCell.stotramAudioBtn.tag = indexPath.row
         stotramCell.stotramAudioBtn.isHidden = true
@@ -68,20 +77,32 @@ class StotramViewController: UIViewController, UITableViewDataSource, UITableVie
     }
     
     func stotramPlay(selectedStotram : Int){
-        if isStotramPlaying && selectedStotramIndex == selectedStotram {
-            stotramPause()
-            return
+        if isStotramPlaying {
+            if selectedStotramIndex == selectedStotram {
+                stotramPause()
+                return
+            } else {
+                stotramTableView.frame.size.height = stotramTableView.frame.size.height + 70
+            }
         }
+        
         isStotramPlaying = true
         selectedStotramIndex = selectedStotram
         stotramBtmPlayBtn.setImage(UIImage(named: "pause"), for: UIControlState.normal)
         stotramTableView.frame.size.height = stotramTableView.frame.size.height - 70
+        
+        //Audio Bottom View
+        
+        stotramPoojaName.text = langStotrams[selectedStotram]
+
         stotramBottomAudioCtlr.isHidden = false
         stotramTableView.reloadData()
     }
     
     func stotramPause(){
         isStotramPlaying = false
+        stotramBottomAudioCtlr.isHidden = true
+        stotramTableView.frame.size.height = stotramTableView.frame.size.height + 70
         stotramBtmPlayBtn.setImage(UIImage(named: "play"), for: UIControlState.normal)
         stotramTableView.reloadData()
     }
